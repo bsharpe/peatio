809785cb3dfa12019c2dfc96a7a1e7987da853de
Ben Sharpe <bsharpe@gmail.com>
2018-01-28 11:08:22 -0700

    get specs passing

--- spec/routing/trade_spec.rb
+++ spec/routing/trade_spec.rb
@@ -1,13 +1,17 @@
 require "spec_helper"
 
-describe "routes for trade" do
+describe "routes for trade", type: :routing do
 
   it "routes /markets/xxxyyy to the trade controller" do
-    Market.expects(:find_by_id).with('xxxyyy').returns(Market.new(id: 'xxxyyy', base_unit: 'xxx', quote_unit: 'yyy'))
-    { :get => "/markets/xxxyyy" }.should be_routable
+    expect(Market).to receive(:find_by_id).with('xxxyyy').and_return(
+      Market.new(id: 'xxxyyy', base_unit: 'xxx', quote_unit: 'yyy')
+    )
+    expect(get "/markets/xxxyyy").to be_routable
+  end
 
-    Market.expects(:find_by_id).with('yyyxxx').returns(nil)
-    { :get => "/markets/yyyxxx" }.should_not be_routable
+  it "does NOT route /markets/yyyxxx" do
+    expect(Market).to receive(:find_by_id).with('yyyxxx').and_return(nil)
+    expect( get "/markets/yyyxxx" ).not_to be_routable
   end
 
 end

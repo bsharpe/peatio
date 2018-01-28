9f2f01394aa6d956e604447bd4c143e1b10c9bc4
Ben Sharpe <bsharpe@gmail.com>
2018-01-28 11:14:32 -0700

    specs passing

--- spec/lib/doorkeeper/access_token_spec.rb
+++ spec/lib/doorkeeper/access_token_spec.rb
@@ -9,15 +9,15 @@ describe Doorkeeper::AccessToken do
 
   context "creation" do
     it "should generate corresponding api token" do
-      lambda {
+      expect {
         Doorkeeper::AccessToken.create!(application_id: app.id, resource_owner_id: member.id, scopes: 'identity', expires_in: 1.week)
-      }.should change(APIToken, :count).by(1)
+      }.to change(APIToken, :count).by(1)
     end
 
     it "should prevent app requesting all scopes" do
-      lambda {
+      expect {
         Doorkeeper::AccessToken.create!(application_id: app.id, resource_owner_id: member.id, scopes: 'all', expires_in: 1.week)
-      }.should raise_error
+      }.to raise_error(RuntimeError)
     end
 
     it "should set token" do
@@ -28,7 +28,7 @@ describe Doorkeeper::AccessToken do
       api_token = APIToken.last
       expect(api_token.label).to eq app.name
       expect(api_token.scopes).to eq %w(identity)
-      api_token.expire_at.should_not be_nil
+      expect(api_token.expire_at).not_to be_nil
     end
 
     it "should link api token" do
@@ -39,15 +39,15 @@ describe Doorkeeper::AccessToken do
   context "revoke" do
     it "should revoke access token and destroy corresponding api token" do
       subject.revoke
-      subject.should be_revoked
-      APIToken.find_by_id(subject.api_token.id).should be_nil
+      expect(subject).to be_revoked
+      expect(APIToken.find_by_id(subject.api_token.id)).to be_nil
     end
   end
 
   context "deletion" do
     it "should soft delete record" do
       subject.destroy
-      Doorkeeper::AccessToken.find_by_id(subject.id).should be_nil
+      expect(Doorkeeper::AccessToken.find_by_id(subject.id)).to be_nil
       expect(Doorkeeper::AccessToken.with_deleted.find_by_id(subject.id)).to eq subject
     end
   end
